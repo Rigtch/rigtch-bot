4bbcdc94e995ab219874854a5fb5116ce7d5eca0
Mnigos <makowskiigor@gmail.com>
2023-03-24 12:18:54 +0100

    feat(commands/types): move `CommandData` to seperate type

--- src/commands/types/index.ts
+++ src/commands/types/index.ts
@@ -7,11 +7,13 @@ import {
 import { WithOptional } from '~/types'
 
 export abstract class Command {
-  public data: WithOptional<
-    SlashCommandBuilder,
-    'addSubcommand' | 'addSubcommandGroup'
-  >
+  public data: CommandData
   public execute: (
     interaction: ChatInputCommandInteraction<CacheType>
   ) => Promise<void>
 }
+
+export type CommandData = WithOptional<
+  SlashCommandBuilder,
+  'addSubcommand' | 'addSubcommandGroup'
+>
