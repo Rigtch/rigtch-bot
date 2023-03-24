a1e04fed0f947ec1bdb534919e5e743b82e3335f
Mnigos <makowskiigor@gmail.com>
2023-03-24 10:09:11 +0100

    test(ping.command): test command data and execute method

---
+++ src/commands/ping.command.spec.ts
@@ -0,0 +1,30 @@
+import { mock } from 'vitest-mock-extended'
+import { beforeEach, describe, expect, test } from 'vitest'
+import { CacheType, ChatInputCommandInteraction } from 'discord.js'
+
+import { PingCommand } from './ping.command'
+
+describe('Command - Ping', () => {
+  let pingCommand: PingCommand
+
+  beforeEach(() => {
+    pingCommand = new PingCommand()
+  })
+
+  describe('data', () => {
+    test('should have name and description', () => {
+      expect(pingCommand.data.name).toEqual('ping')
+      expect(pingCommand.data.description).toEqual('Replies with Pong!')
+    })
+  })
+
+  describe('execute', () => {
+    test('should reply with Pong!', async () => {
+      const interaction = mock<ChatInputCommandInteraction<CacheType>>()
+
+      await pingCommand.execute(interaction)
+
+      expect(interaction.reply).toHaveBeenCalledWith('Pong!')
+    })
+  })
+})
