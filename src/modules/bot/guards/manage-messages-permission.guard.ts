6da7d7e918044b9c03801af5ff48cb76a6fe53fc
Mnigos <makowskiigor@gmail.com>
2023-08-23 14:44:26 +0200

    refactor(modules/bot): remove unnecesary `dtos`, `commands` and `guards`

--- src/modules/bot/guards/manage-messages-permission.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common'
-
-export class ManageMessagesPermissionGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
-    console.log(context)
-
-    return true
-  }
-}
